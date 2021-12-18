f43daec39749b844bb3d2b1aa2c823c495f61510
REDDEVIL <tosha.kostylev.97@mail.ru>
2021-12-19 02:57:11 +0300

    4.1. Разбор первой программы. [#159033]

---
+++ src/main/java/ru/job4j/StudentInfo.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class StudentInfo {
+    public static void main(String[] args) {
+        System.out.println("Anton Kostylev");
+        System.out.println("31.08.2021");
+    }
+}
