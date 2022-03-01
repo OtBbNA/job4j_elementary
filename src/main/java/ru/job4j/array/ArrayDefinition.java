7f02f02229add6e12766a51a9a86b155e98d1630
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-01 14:21:20 +0300

    6.0.1.1. Размер массива [#333582]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -8,5 +8,14 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен 2: " + surnames.length);
         float[] prices = new float[40];
         System.out.println("Размер массива равен 3: " + prices.length);
+        String[] names = new String[4];
+        names[0] = "Гриша";
+        names[1] = "Миша";
+        names[2] = "Саша";
+        names[3] = "Флера";
+        System.out.println(names[0]);
+        System.out.println(names[1]);
+        System.out.println(names[2]);
+        System.out.println(names[3]);
     }
 }
