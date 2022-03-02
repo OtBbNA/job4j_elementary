af756f3d2ee8e1db8931b88b1f18728a4dd3ee63
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-03-02 19:22:07 +0300

    6.6.1. Двумерный массив [#226796]

--- src/main/java/ru/job4j/array/Sequence.java
+++ src/main/java/ru/job4j/array/Sequence.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 public class Sequence {
     public static void main(String[] args) {
-        int[][] array = new int[3][3];
+        int[][] array = new int[2][2];
         array[0][0] = 1;
         array[1][0] = 2;
         array[2][0] = 3;
