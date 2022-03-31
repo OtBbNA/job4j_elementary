216aee599bf149a01e3e30d43af0b0c88425923f
REDDEVIL <tosha.kostylev.97@mail.ru>
2022-04-01 00:02:29 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,20 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport{
+
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int num) {
+        num = 16;
+    }
+
+    @Override
+    public double refuel(double gal) {
+        double money = gal * 6.52;
+        return money;
+    }
+}
