ce42a50d35210ae4f6690292a48c859a16296db4
REDDEVIL <tosha.kostylev.97@mail.ru>
2025-04-16 19:24:14 +0300

    6. Правила Checkstyle [#504981]

---
+++ src/main/java/ru/job4j/checkstyle/Broken.java
@@ -0,0 +1,24 @@
+package ru.job4j.checkstyle;
+
+public class Broken {
+
+    public final int sizeOfEmpty = 10;
+    public static final String NEW_VALUE = "";
+
+    private String surname;
+    private String name;
+
+    void echo() {
+
+    }
+
+    void media(Object obj) {
+        if (obj != null) {
+            System.out.println(obj);
+        }
+    }
+
+    void method(int a, int b, int c, int d, int e, int f, int g) {
+
+    }
+}
